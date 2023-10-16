b87e19270df000666f5eb17893187d0cc4a909f8
Melroy <melroyr@yahoo.com>
2023-10-16 11:36:25 -0400

    Initial Checkin

---
+++ hello-world-backend/src/main/java/com/myco/helloworld/backend/HelloWorldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myco.helloworld.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldBackendApplication.class, args);
+	}
+
+}
